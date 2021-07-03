df39623be383394f6df31808ca9e46639c3204a5
Humza Sajid <pak786hamzasajid66@Gmail.com>
2021-07-03 18:56:53 +0500

    Add files via upload

---
+++ TASKS - 5 - Arrays/app.js
@@ -0,0 +1,79 @@
+//----TASK-1------
+//Traversing
+
+var arr=[4, -20, 50, 1, 0, -5, 6, 7, 10];
+
+for(var i=0; i<arr.length; i++)
+{
+  console.log("At Index"+ i+ " value is : "+arr[i]);
+}
+
+
+//Task-2
+
+function task2() {
+
+var arr2=[9, 7 , 75, 32, 3234, 5, 3];
+
+for(var a=0; a<arr2.length; a++)
+{
+  if (arr2.includes(3)) {
+  return true;
+  }
+else {
+return false;
+}}}
+
+var abc=task2();
+console.log(abc);
+
+
+
+//----TASK-3----------
+
+
+
+
+function task3() {
+
+var arr3= [2, 2, 1, 0, 9, 39, 20] ;
+var number=0;
+for(var b=0; b<arr3.length; b++)
+{
+  if (arr3.includes(2))
+  {
+    number=number+1;
+    console.log(number);
+  return number;
+  }
+else {
+return 0;
+}}}
+
+var abcd=task3();
+console.log(abcd);
+
+
+
+//Task---4------
+var arr4= [2, 3, 0, 1, 4, 6, 7, 7];
+arr4[2]=200;
+
+//-----task-5--------
+var arr5=[1, 9, 20, 20, 4, 0, 3, 4];
+
+arr5=arr5.push(9);
+
+//---task-6--------
+var arr6=[0, 3, 4, 5, 6, 9, 0];
+
+arr6=arr6.unshift(1);
+
+//---task-7------
+var array=[3, 8, 10, 12, 45, 34, 34, 34, 2, 2, 0];
+
+array.splice(3, 0, 7);
+
+for (var i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
