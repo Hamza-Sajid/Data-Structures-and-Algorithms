1bbc43b2d2d9dee491e6201508e9493ba56c6d3b
Humza Sajid <pak786hamzasajid66@Gmail.com>
2021-07-16 10:38:57 +0500

    Update task1.js

--- TASKS - 7  | STACKS/task1.js
+++ TASKS - 7  | STACKS/task1.js
@@ -1,1 +1,18 @@
 
+class Stack{
+    constructor(){
+        this.items = ["This","is","Humza"];
+        this.size = 15;
+    }
+    printStack(){
+
+        for (let i=0; i < this.items.length; i++){
+            console.log(this.items[i]);
+        }
+    }
+    size(){
+        return this.size;
+    }
+}
+
+let myStack = new Stack();
